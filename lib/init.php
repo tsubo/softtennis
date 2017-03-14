ff2575613f91aa5158ebc23c94eeb4b275373f79
tsubo <ktsubota@gmail.com>
2017-03-14 16:55:20 +0900

    make mega menu

--- lib/init.php
+++ lib/init.php
@@ -6,6 +6,7 @@ require_once './vendor/autoload.php';
  */
 // assets ファイル格納先 URL（Future Shop の SSL 領域に assets ファイルを格納している）
 define('ASSETS_URL', 'https://secure1.future-shop.jp/~softtennis');
+define('CATEGORIES_URL', 'http://www.softtennis-lafino.jp/fs/softtennis/c');
 
 /*
  * Twig テンプレートエンジン
@@ -26,6 +27,14 @@ $function = new Twig_SimpleFunction('ssl_url', function($path) {
     $path = '/' . $path;
   }
   return ASSETS_URL . $path;
+});
+$twig->addFunction($function);
+
+$function = new Twig_SimpleFunction('cat_url', function($path) {
+  if (substr($path, 0, 1) != '/') {
+    $path = '/' . $path;
+  }
+  return CATEGORIES_URL . $path;
 });
 $twig->addFunction($function);
 
