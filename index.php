44a755ef3a1c3e65449c5ea530e9c4178b29a1e8
tsubo <ktsubota@gmail.com>
2017-03-24 00:54:22 +0900

    yaml ファイルでデータを管理する機能を追加
    おしらせ、ピックアップ、新着商品を yaml ファイルで管理できるようにした

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
+use Symfony\Component\Yaml\Yaml;
+
 // Twig テンプレートエンジン
 $twig = require_once('./lib/init.php');
 
 // ブログ RDF 取得
 $rdf = simplexml_load_file('http://lafino-softtennis.officialblog.jp/index.rdf');
 
-echo $twig->render('pages/home/home.html.twig', array('rdf' => $rdf));
+// 設定ファイル読込
+$datas = Yaml::parse(file_get_contents('./config/home.yml'));
+
+echo $twig->render('pages/home/home.html.twig', compact('rdf', 'datas'));
